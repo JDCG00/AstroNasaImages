88266e9a963131281e3d416526356467db0cd285
JDCG00 <jcarreterogranado.guadalupe@alumnado.fundacionloyola.net>
2023-08-30 09:17:40 +0000

    first commit

---
+++ .netlify/functions-internal/chunks/index@_@astro.487f14ef.mjs
@@ -0,0 +1,14 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+import './astro.718b4cdf.mjs';
+import 'html-escaper';
+import 'cookie';
+import 'kleur/colors';
+import '@astrojs/internal-helpers/path';
+import 'path-to-regexp';
+import 'mime';
+import 'string-width';
+
+const page = () => import('./pages/index.astro.81b257de.mjs');
+
+export { page };
