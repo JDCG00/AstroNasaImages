2516dc90cda984d8bb044243181d6a8c126cf31a
JDCG00 <jcarreterogranado.guadalupe@alumnado.fundacionloyola.net>
2023-08-30 10:04:24 +0000

    deploy

---
+++ dist/server/chunks/index@_@astro.d6d204a0.mjs
@@ -0,0 +1,17 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+import './astro.7747b10c.mjs';
+import 'html-escaper';
+import 'path-to-regexp';
+import 'cookie';
+import '@astrojs/internal-helpers/path';
+import 'kleur/colors';
+import 'node:fs';
+import 'node:http';
+import 'node:tls';
+import 'mime';
+import 'string-width';
+
+const page = () => import('./pages/index.astro.6a095f22.mjs');
+
+export { page };
